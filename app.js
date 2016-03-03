f3d43956b642365c7a8f22188871e39429ce231b
samonakuba <samonakuba@ubuntu.(none)>
2016-03-03 08:06:55 -0800

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var http = require('http');
 var app = http.createServer(handler);
 
-app.listen(8080);
+app.listen(80);
 
 function handler (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
